63ce403905b783afeca76bb1757b42d0fba047c4
felipe <felipesilvasantoslawliet1@gmail.com>
2023-02-01 11:02:08 -0300

    feat: add JSON stringify for showing the users and header for change the content type

--- src/server.js
+++ src/server.js
@@ -1,18 +1,27 @@
-import http from 'node:http';
+import http from "node:http";
+
+const users = [];
 
 const server = http.createServer((req, res) => {
   const { method, url } = req;
 
-  if (method === 'GET' && url === '/users') {
-    return res.end('Listagem de usuários')
+  if (method === "GET" && url === "/users") {
+    return res
+      .setHeader("Content-Type", "application/json")
+      .end(JSON.stringify(users));
   }
 
-  if (method === 'POST' && url === '/users') {
-    return res.end('Criação de usuários')
+  if (method === "POST" && url === "/users") {
+    users.push({
+      id: 1,
+      name: "eu mesmo",
+      email: "eumesmo@gmail.com",
+    });
+
+    return res.end("Criação de usuários");
   }
 
-  return res.end('Hello World!')
-})
+  return res.end("Hello World!");
+});
 
-server.listen(3333)
-
+server.listen(3333);
